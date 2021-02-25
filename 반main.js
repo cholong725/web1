5697ae851c19a98ad5c740b0979b8e713e7d2a77
cholong725 <78252840+cholong725@users.noreply.github.com>
2021-02-25 19:45:15 +0900

    Delete 반main.js

--- 반main.js
+++
@@ -1,9 +0,0 @@
-
-const toggleBtn = document.querySelector('.nav1__toggleBtn');
-const menu = document.querySelector('.nav1__menu');
-const icons = document.querySelector('.nav1__icons');
-
-toggleBtn.addEventListener('click', () => {
-  menu.classList.toggle('active');
-  icons.classList.toggle('active');
-});
